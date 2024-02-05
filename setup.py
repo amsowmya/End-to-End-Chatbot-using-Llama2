46b77c5870bc49adbaf752a7f5732793940301c4
amsowmya <soumya.pdit@gmail.com>
2024-02-05 13:36:16 +0530

    Created e2e medical chatbot application

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = 'Medical Chatbot',
+    version = '0.0.1',
+    author = 'Sowmya AM',
+    author_email = 'sowmya.anekonda@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
